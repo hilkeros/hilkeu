69b7a001bc90885787b888a5586c96fb5ba27789
Hilke Ros <hilkeros@gmail.com>
2021-05-01 16:17:17 +0200

    add video chat basics

--- public/greta/sketch.js
+++ public/greta/sketch.js
@@ -30,6 +30,68 @@ let heart;
 let showHearts = false;
 let system;
 
+//socket stuff
+const ROOM_ID='greta'
+const socket = io('/')
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const videoGrid = document.getElementById('video-grid')
+const peers = {}
+const myPeer = new Peer(undefined, {
+  path: '/peerjs/server',
+  port: '3000',
+  host: 'localhost'
+})
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: false
+}).then(stream => {
+  addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+///
+
 function preload() {
     song = loadSound(songUrl);
     heart = loadImage('./images/heart.png');
@@ -45,8 +107,9 @@ function setup() {
 
     texts.map(createPost);
 
-    musicButton = createButton('Play music').parent('controls').mousePressed(toggleSong);;
+    musicButton = createButton('Play music').parent('controls').mousePressed(toggleSong);
     createButton('Love').parent('controls').mousePressed(toggleLove);
+    createButton('Start camera').parent('controls').mousePressed(turnOnVideo);
 
     system = new ParticleSystem(createVector(width / 2, 100));
 
@@ -123,6 +186,11 @@ function createPost(text, index) {
     return postWrapper;
 }
 
+///
+/// PARTICLE LOGIC
+///
+
+
 // A simple Particle class
 let Particle = function(position) {
     this.acceleration = createVector(0, 0.05);
@@ -176,4 +244,3 @@ let Particle = function(position) {
       }
     }
   };
-
--- server.js
+++ server.js
@@ -7,28 +7,39 @@ const host = process.env.BASE_URL || 'localhost'
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 
+app.get('/hi', (req, res) => {
+    res.redirect(`/hi/${uuidV4()}`)
+})
+
+app.get('/hi/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room, port: port.toString(), host: host })
+})
+
 app.get('/greta', (req, res) => {
-  res.render('greta', { roomId: 'greta', port: port.toString(), host: host })
+    res.render('greta', { port: port.toString(), host: host })
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+          socket.to(roomId).broadcast.emit('user-connected', userId)
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+          socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+          })
     })
-  })
 })
 
 server.listen(port)
 
 const peerServer = ExpressPeerServer(server, {
-  path: '/server'
+    path: '/server'
 })
+
+app.use('/peerjs', peerServer)
