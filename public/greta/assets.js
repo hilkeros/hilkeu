a9549d26bb67ae3db447a361d4f8448f0a4086f7
Hilke Ros <hilkeros@gmail.com>
2021-05-26 17:04:17 +0200

    make syntax consistent

--- public/greta/assets.js
+++ public/greta/assets.js
@@ -16,8 +16,8 @@ const texts = [
     `Love me, tell me. I don't think that you are doing fine`,
     `Hold me, sorry. It's not that easy to cross the line`,
     `FOR THE PEOPLE LOSING TIME`,
-];
+]
 
-const videoUrl = 'https://dl.dropboxusercontent.com/s/rf2mywto62mkb4s/hilke-trimmed.mov';
-const videoUrl2 = 'https://dl.dropboxusercontent.com/s/oe2qqi3qf0iarqa/aline.mov?dl=0';
-const songUrl = 'https://dl.dropboxusercontent.com/s/zkhoe2ujn2h0uxa/greta.mp3';
+const videoUrl = 'https://dl.dropboxusercontent.com/s/rf2mywto62mkb4s/hilke-trimmed.mov'
+const videoUrl2 = 'https://dl.dropboxusercontent.com/s/oe2qqi3qf0iarqa/aline.mov?dl=0'
+const songUrl = 'https://dl.dropboxusercontent.com/s/zkhoe2ujn2h0uxa/greta.mp3'
--- public/greta/sketch.js
+++ public/greta/sketch.js
@@ -1,213 +1,212 @@
-let canvas;
+let canvas
 
-const { Engine, World, Bodies, Svg, Vertices } = Matter;
-let engine;
-let world;
-let hearts = [];
-let ground;
-let heartImage, purpleHeartImage, blueHeartImage;
-let heartVertices;
-let path;
-let logo;
-let header;
+const { Engine, World, Bodies, Svg, Vertices } = Matter
+let engine
+let world
+let hearts = []
+let ground
+let heartImage, purpleHeartImage, blueHeartImage
+let heartVertices
+let path
+let logo
+let header
 
-let song;
-let musicButton;
+let song
+let musicButton
 
-let videos = [];
-let stroboOn = false;
-let backgroundIsWhite = false;
+let videos = []
+let stroboOn = false
+let backgroundIsWhite = false
 
 function preload() {
-  heartImage = loadImage('images/heart.png');
-  purpleHeartImage = loadImage('images/heartpurple.png');
-  blueHeartImage = loadImage('images/heartblue.png');
+  heartImage = loadImage('images/heart.png')
+  purpleHeartImage = loadImage('images/heartpurple.png')
+  blueHeartImage = loadImage('images/heartblue.png')
   
-  song = loadSound(songUrl);
+  song = loadSound(songUrl)
 }
 
 function setup() {
-  const withCameraButton = select('#start-with-camera');
-  withCameraButton.show();
+  const withCameraButton = select('#start-with-camera')
+  withCameraButton.show()
   const withoutCameraButton = select('#start-without-camera')
-  withoutCameraButton.show();
-  withCameraButton.mousePressed(hideStartModalAndStartCamera);
-  withoutCameraButton.mousePressed(hideStartModal);
+  withoutCameraButton.show()
+  withCameraButton.mousePressed(hideStartModalAndStartCamera)
+  withoutCameraButton.mousePressed(hideStartModal)
 
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent('canvas-holder');
-  rectMode(CENTER);
-  engine = Engine.create();
-  world = engine.world;
+  canvas = createCanvas(windowWidth, windowHeight)
+  canvas.parent('canvas-holder')
+  rectMode(CENTER)
+  engine = Engine.create()
+  world = engine.world
   let options = {
     isStatic: true
-  };
+  }
 
-  ground = Bodies.rectangle(width / 2, height - 2, width, 8, options);
-  World.add(world, ground);
-  path = document.getElementById('heart-path');
-  heartVertices = Svg.pathToVertices(path);
-  heartVertices = decomp.quickDecomp(heartVertices);
+  ground = Bodies.rectangle(width / 2, height - 2, width, 8, options)
+  World.add(world, ground)
+  path = document.getElementById('heart-path')
+  heartVertices = Svg.pathToVertices(path)
+  heartVertices = decomp.quickDecomp(heartVertices)
 
-  texts.map(createPost);
+  texts.map(createPost)
 
-  let loveButton = select('#love');
-  loveButton.mousePressed(showHearts);
+  let loveButton = select('#love')
+  loveButton.mousePressed(showHearts)
 
-  musicButton = select('#music');
-  musicButton.mousePressed(toggleSong);
+  musicButton = select('#music')
+  musicButton.mousePressed(toggleSong)
 
-  let cameraButton = select('#camera');
-  cameraButton.mousePressed(turnOnVideo);
+  let cameraButton = select('#camera')
+  cameraButton.mousePressed(turnOnVideo)
 
-  let stroboButton = select('#share');
-  stroboButton.mousePressed(toggleStrobo);
+  let stroboButton = select('#share')
+  stroboButton.mousePressed(toggleStrobo)
 
-  logo = select('.logo');
-  header = select('.header');
+  logo = select('.logo')
+  header = select('.header')
 
-  // song.addCue(2, showHearts);
-  // song.addCue(5, hideHearts);
-  song.addCue(83.5, showHearts);
-  song.addCue(88.8, hideHearts);
-  song.addCue(94, showHearts);
-  song.addCue(99.2, hideHearts);
-  song.addCue(104.5, toggleStrobo);
-  song.addCue(125.2, toggleStrobo);
-  song.addCue(187, showHearts);
-  song.addCue(192.2, hideHearts);
-  song.addCue(198.2, showHearts);
-  song.addCue(203.8, hideHearts);
-  song.addCue(208.5, toggleStrobo);
-  song.addCue(229.5, toggleStrobo);
+  // song.addCue(2, showHearts)
+  // song.addCue(5, hideHearts)
+  song.addCue(83.5, showHearts)
+  song.addCue(88.8, hideHearts)
+  song.addCue(94, showHearts)
+  song.addCue(99.2, hideHearts)
+  song.addCue(104.5, toggleStrobo)
+  song.addCue(125.2, toggleStrobo)
+  song.addCue(187, showHearts)
+  song.addCue(192.2, hideHearts)
+  song.addCue(198.2, showHearts)
+  song.addCue(203.8, hideHearts)
+  song.addCue(208.5, toggleStrobo)
+  song.addCue(229.5, toggleStrobo)
 
-  song.onended(songEnded);
+  song.onended(songEnded)
 }
 
 function draw() {
   if (stroboOn) {
 
     if (frameCount % 22 === 0) {
-      toggleBackground();
+      toggleBackground()
     }
 
     if (backgroundIsWhite) {
-      background('rgba(255, 255, 255, 0.05)');
+      background('rgba(255, 255, 255, 0.05)')
     } else {
-      clear();
+      clear()
     }
   } else {
-    clear();
+    clear()
   }
 
-  Engine.update(engine, 1000 / 70);
+  Engine.update(engine, 1000 / 70)
 
   hearts.map((heart, index) => {
-    heart.show();
+    heart.show()
     if (heart.isDead()) {
-      heart.die();
-      hearts.splice(index, 1);
+      heart.die()
+      hearts.splice(index, 1)
     }
-  });
+  })
 
-  noStroke();
-  fill(255);
-  rect(width / 2, height - 10, width, 16);
+  noStroke()
+  fill(255)
+  rect(width / 2, height - 10, width, 16)
 
   if (song.isPlaying()) {
     if (frameCount % 60 === 0) {
-      videos.map(sync);
+      videos.map(sync)
     }
   }
   
 }
 
 windowResized = function() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight)
 }
 
 function hideStartModal() {
-  select('#start').hide();
-  select('.header').style('display', 'flex');
-  select('.container').style('display', 'flex');
+  select('#start').hide()
+  select('.header').style('display', 'flex')
+  select('.container').style('display', 'flex')
 }
 
 function hideStartModalAndStartCamera() {
-  hideStartModal();
-  turnOnVideo();
+  hideStartModal()
+  turnOnVideo()
 }
 
 function showHearts() {
-  for (let i = 0; i < 6; i++) {
-    hearts.push(new Heart(random(width / 2) + width / 4, random(height / 5), 200));
+  for (let i = 0 ; i < 6 ; i++) {
+    hearts.push(new Heart(random(width / 2) + width / 4, random(height / 5), 200))
   }
 
-  logo.html('lovebook');
-  header.addClass('love');
-  setTimeout(hideHearts, 4000);
+  logo.html('lovebook')
+  header.addClass('love')
+  setTimeout(hideHearts, 4000)
 }
 
 function hideHearts() {
-  logo.html('hatebook');
-  header.removeClass('love');
+  logo.html('hatebook')
+  header.removeClass('love')
 }
 
 function toggleBackground() {
-  backgroundIsWhite = !backgroundIsWhite;
+  backgroundIsWhite = !backgroundIsWhite
 }
 
 function toggleStrobo() {
-  stroboOn = !stroboOn;
+  stroboOn = !stroboOn
 }
 
 function toggleSong() {
   if (song.isPlaying()) {
-    song.pause();
-    videos.map(video => video.pause());
-    musicButton.html('play music');
+    song.pause()
+    videos.map(video => video.pause())
+    musicButton.html('play music')
 
   } else {
-    song.play();
-    videos.map(video => video.play());
-    musicButton.html('pause music');
+    song.play()
+    videos.map(video => video.play())
+    musicButton.html('pause music')
   }
 }
 
 function songEnded() {
-  musicButton.html('play music');
+  musicButton.html('play music')
 }
 
 function sync(video) {
-  const drift = abs(song.currentTime() - video.time());
+  const drift = abs(song.currentTime() - video.time())
   if (drift > 0.1) {
-    video.time(song.currentTime());
+    video.time(song.currentTime())
   }
 }
 
 function createPost(text, index) {
-  const even = index % 2 === 0;
-  const userName = even ? 'hilke' : 'gregory';
-  const profilePic = even ? 'images/hilke.png' : 'images/gregory.png';
-  const url = even ? videoUrl : videoUrl2;
+  const even = index % 2 === 0
+  const userName = even ? 'hilke' : 'gregory'
+  const profilePic = even ? 'images/hilke.png' : 'images/gregory.png'
+  const url = even ? videoUrl : videoUrl2
 
-  const postWrapper = createDiv().parent('#posts').class('post');
-  const userWrapper = createDiv().parent(postWrapper).class('user-wrapper');
-  createImg(profilePic, userName).parent(userWrapper).class('profile-pic');
-  createSpan(userName).parent(userWrapper).class('user-name');
-  createDiv(text).parent(postWrapper).class('text');
+  const postWrapper = createDiv().parent('#posts').class('post')
+  const userWrapper = createDiv().parent(postWrapper).class('user-wrapper')
+  createImg(profilePic, userName).parent(userWrapper).class('profile-pic')
+  createSpan(userName).parent(userWrapper).class('user-name')
+  createDiv(text).parent(postWrapper).class('text')
 
-  let videoWrapper = createDiv().parent(postWrapper).class('video');
-  let myVideo = createVideo(url);
-  myVideo.parent(videoWrapper);
-  myVideo.volume(0);
-  videos.push(myVideo);
+  let videoWrapper = createDiv().parent(postWrapper).class('video')
+  let myVideo = createVideo(url)
+  myVideo.parent(videoWrapper)
+  myVideo.volume(0)
+  videos.push(myVideo)
 
-  return postWrapper;
+  return postWrapper
 }
 
 
 
 /* TO DO:
-- better syncing of videos after many pauses
 - different heart sizes
 - gradual colors */
